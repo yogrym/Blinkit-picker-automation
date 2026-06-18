1310aea9129de43129c43766b4fc2da67783610e
grym <voidgrym@pm.me>
2026-06-18 20:37:16 +0530

    Reapply "Intial commits"
    This reverts commit f53f78d7e7c8da4f5cfe0d20811c2780f2d17cd8.

---
+++ src/main/java/com/picker/BlinkitPicker/BlinkitPickerApplication.java
@@ -0,0 +1,13 @@
+package com.picker.BlinkitPicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlinkitPickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlinkitPickerApplication.class, args);
+	}
+
+}
